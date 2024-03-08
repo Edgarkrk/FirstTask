fcf97c9a7ba22523f1605abd6298e3b71dd6b1c2
Edhar Kruk <edgarkrook@gmail.com>
2024-03-08 16:46:11 +0100

    ok

---
+++ First.pr/First.pr/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+int number = 5;
+
+var variable = 1;
+
+var url = string.Empty;
+int[] arraysNumber = {1,2,3,4};
+double[] doubleNumbers = { 0.1, 0.2, 0.3 };
+
+
+
+void Foo(int n1)
+{
+    
+}
+
+int ExampleNumber()
+{
+    return 1;
+}
